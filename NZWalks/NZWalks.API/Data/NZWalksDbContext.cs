4ebafe2b8bec9e44d68500325a19c43af4d72f44
alexanderbeh89 <Alexander.Beh@sandisk.com>
2025-10-08 18:16:21 +0800

    Commit for Module 2 training.

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Models.Domain.Difficulty> Difficulties { get; set; }
+        public DbSet<Models.Domain.Region> Regions { get; set; }
+        public DbSet<Models.Domain.Walk> Walks { get; set; }
+    }
+}
